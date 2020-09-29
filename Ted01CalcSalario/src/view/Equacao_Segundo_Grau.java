b7abe26fe2e7790fa79688f531bbd6703fb9d7d9
Rodrigo Andrade <rdghenrique94@gmail.com>
2020-09-29 11:39:24 -0300

    Update Calculo Salario

---
+++ Ted01CalcSalario/src/view/Equacao_Segundo_Grau.java
@@ -0,0 +1,25 @@
+package view;
+
+public class Equacao_Segundo_Grau {
+
+	public static void main(String[] args) {
+		int a = 1;
+		int b = -2;
+		int c = -3;
+		double delta;
+		double x1;
+		double x2;
+
+		delta = (b^b) - 4*a*c; 
+
+		x1 = (-b + (Math.sqrt((b^b) - 4 * a * c))) / (2 * a);
+
+		x2 = (-b - (Math.sqrt((b^b) - 4 * a * c))) / (2 * a); 
+
+		System.out.printf("Valor de x1: %.2f%n",x1);
+		System.out.printf("Valor de x2: %.2f%n",x2);
+		System.out.printf("Valor de Delta: %.2f%n",delta);
+
+	}
+
+}
---
+++ Ted01CalcSalario/src/view/Teste.java
@@ -0,0 +1,30 @@
+package view;
+
+import java.util.Scanner;
+
+import model.Salario_Bruto;
+
+public class Teste {
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		Salario_Bruto salario = new Salario_Bruto();
+		
+		System.out.println("Enter Hours Value and Hours Works: ");
+		System.out.print("Hours Value: ");
+		salario.hourValue = sc.nextDouble();
+		System.out.println();
+		System.out.print("Hours Works: ");
+		salario.hourWork = sc.nextDouble();
+		System.out.println();
+		System.out.printf("Gross Salary %.2f%n ",salario.calcSalario());
+		System.out.printf("Salary discount IR %.2f%n",salario.calcIr());
+		System.out.printf("Salary discount IN %.2f%n",salario.calcIn());
+		System.out.printf("Salary discount SI %.2f%n",salario.calcSn());
+		
+		
+		sc.close();
+	}
+	
+
+}
